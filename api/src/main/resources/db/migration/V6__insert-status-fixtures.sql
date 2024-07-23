a50c893018a8fc34bd516a6fbd5089bb2955d462
OtavioESP <otavio_emilio@outlook.com>
2024-07-23 18:26:16 -0300

    feat: fixture for status

---
+++ api/src/main/resources/db/migration/V6__insert-status-fixtures.sql
@@ -0,0 +1,8 @@
+INSERT INTO status (nome, codigo, cor)
+VALUES
+('Backlog', 1, '#6b6b6b'),
+('Sprint', 2, '#cce0ff'),
+('Development', 3, '#428cfc'),
+('Revision', 4, '#5f1b63'),
+('Homolog', 5, '#cfb44a'),
+('Production', 6, '#31b51d');
